1c874613f38abc757f8a05fca382976966f56a9a
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-09-30 14:23:47 +0700

    test: testing HashRouter

--- src/Pages/SidebarMain.js
+++ src/Pages/SidebarMain.js
@@ -20,14 +20,14 @@ function SidebarMain() {
     <Sidebar>
       <Logo_content>
         <Logo>
-          <i class='bx bxs-cat'></i>
+          <i className='bx bxs-cat'></i>
           <Logo_name>PANKORN.WANG</Logo_name>
         </Logo>
       </Logo_content>
       <Sidebar_ul>
         <Sidebar_li onClick={() => navigate('/')}>
           <Side_a as={Link}>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>Home Page</span>
           </Side_a>
         </Sidebar_li>
@@ -36,42 +36,42 @@ function SidebarMain() {
         {/* Space */}
         <Sidebar_li onClick={() => navigate('/programmingskill')}>
           <Side_a as={Link}>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>Skills</span>
           </Side_a>
         </Sidebar_li>
         {/* Space */}
         <Sidebar_li onClick={() => navigate('/experiences')}>
           <Side_a as={Link}>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>Experiences</span>
           </Side_a>
         </Sidebar_li>
         {/* Space */}
         <Sidebar_li onClick={() => navigate('/bookshelf')}>
           <Side_a as={Link}>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>Bookshelf</span>
           </Side_a>
         </Sidebar_li>
         {/* Space */}
         <Sidebar_li onClick={() => navigate('/trading')}>
           <Side_a as={Link}>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>Trading</span>
           </Side_a>
         </Sidebar_li>
         {/* Space */}
         <Sidebar_li onClick={() => navigate('/showcase')}>
           <Side_a as={Link}>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>Showcase</span>
           </Side_a>
         </Sidebar_li>
         {/* Space */}
         <Sidebar_li>
           <Side_a href='#'>
-            <i class='bx bx-spa'></i>
+            <i className='bx bx-spa'></i>
             <span>GitHub & LinkedIn</span>
           </Side_a>
         </Sidebar_li>
